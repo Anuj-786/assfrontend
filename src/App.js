790e921d097e544669626ea69959912da052abb5
Anuj-786 <anuj.k@gai.co.in>
2018-05-07 19:46:47 +0530

    adding files

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          isAuthenticated: true,
+          isAuthenticated: false,
           user: '',
           userId: '',
           bookmarks: [],
