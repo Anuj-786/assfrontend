4f1ccfd2705628100924452a5763bebb73143995
Anuj-786 <anuj.k@gai.co.in>
2018-05-07 19:36:29 +0530

    adding files

--- src/App.js
+++ src/App.js
@@ -7,17 +7,43 @@ class App extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { isAuthenticated: false, user: null};
+        this.state = {
+          isAuthenticated: true,
+          user: '',
+          userId: '',
+          bookmarks: [],
+          bookmark: '',
+        };
     }
 
     logout = () => {
         this.setState({isAuthenticated: false})
+        // var apiKey = '860174696391-8g8slc7sge6rj2jn08e2gpi9q1gafsjb.apps.googleusercontent.com'
     };
     componentDidMount() {
-      this.fetchData();
+      // this._fetchBookmarks();
+      // this._addUser()
     }
-    fetchData = () => {
-      fetch('http://localhost:4001/users')
+
+    _fetchBookmarks = () => {
+      fetch('http://localhost:4001/getbookmarks')
+      .then( (res) => res.json())
+      .then( (data) => {
+        this.setState({bookmarks: data.response})
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    }
+    _addUser = () => {
+      fetch('http://localhost:3002/users', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          email: 'anuj.k',
+          user_id: 'test'
+        })
+      })
       .then( (res) => res.json())
       .then( (data) => {
         console.log(data)
@@ -26,24 +52,65 @@ class App extends Component {
         console.log(error)
       })
     }
+
+
     googleResponse = (res) => {
-      console.log(res)
+      // console.log(res)
       // this.setState({isAuthenticated: true, user: res.w3.U3})
+      this.setState({user: res.WE.w3.U3, userId: res.WE.googleId});
     };
-    
+    _handleInput = (event) => {
+      // console.log(event.target.value);
+      this.setState({bookmark: event.target.value })
+    }
+    _handleSubmit = (event) => {
+      console.log(this.state.bookmark)
+      fetch('http://localhost:3002/savebookmark', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          url: 'testurl',
+          user_id: 87
+        })
+      })
+      .then( (res) => res.json())
+      .then( (data) => {
+        console.log(data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+       event.preventDefault();
+    }
+
     render() {
+        var bookmarksData =  this.state.bookmarks.map((bookmarks) => {
+          return <li key = {bookmarks.id}>{bookmarks.email} </li>
+        });
+        // console.log(this.state.bookmarks)
         let content = !!this.state.isAuthenticated ?
             (
                 <div>
-                    <p>Authenticated</p>
+                    <p>user name</p>
                     <div>
                         {this.state.user}
+                        {this.state.userId}
                     </div>
                     <div>
                         <button onClick={this.logout} className="button">
                             Log out
                         </button>
+                        <form onSubmit={this._handleSubmit}>
+                          <label>
+                            Name:
+                            <input type="text" name="name" onChange={this._handleInput}/>
+                          </label>
+                          <input type="submit" value="Submit" />
+                        </form>
                     </div>
+                    <ul className = "bookmarks-list">
+                      {bookmarksData}
+                    </ul>
                 </div>
             ) :
             (
